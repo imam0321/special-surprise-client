e63fbadcb51fb9f84db9397dd3e77b80c4d9c8eb
imam0321 <imam.hossain0321@gmail.com>
2026-07-30 19:25:12 +0600

    feat: initialize homepage, configure global styles with Tailwind, and setup root layout with fonts and toast notifications

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
   },
 };
 
-export const dynamic = "force-dynamic";
+export const revalidate = 60;
 
 export default async function HomePage() {
   const categories = await getAllCategories("");
